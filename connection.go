b46264dbe254dd5e2c383791f9209533b5140043
CHA-OMEN\caris <chahyun.dev@gmail.com>
2024-10-30 13:09:30 +0900

    [Feat] Player Position 서버 Recive 개발

---
+++ connection.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/binary"
+	"log"
+	"net"
+
+	mg "server-go/src/managers"
+	pb "server-go/src/pb"
+
+	"google.golang.org/protobuf/proto"
+)
+
+// StartConnection: 서버 시작 및 연결 처리
+func StartConnection(listener net.Listener) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+// handleConnection: 클라이언트 연결을 처리하고 엔티티별 서비스에 위임
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		// 메시지 길이를 읽음 (4바이트, Little Endian)
+		lengthBuf := make([]byte, 4)
+		_, err := conn.Read(lengthBuf)
+		if err != nil {
+			log.Println("Failed to read message length:", err)
+			return
+		}
+		length := binary.LittleEndian.Uint32(lengthBuf)
+
+		// 메시지 본문 읽기
+		messageBuf := make([]byte, length)
+		_, err = conn.Read(messageBuf)
+		if err != nil {
+			log.Println("Failed to read message body:", err)
+			return
+		}
+
+		// Protocol Buffers 메시지를 파싱
+		var gameMessage pb.GameMessage
+		if err := proto.Unmarshal(messageBuf, &gameMessage); err != nil {
+			log.Println("Failed to unmarshal message:", err)
+			return
+		}
+
+		// 메시지 처리 및 엔티티로 라우팅
+		processMessage(&gameMessage, conn)
+	}
+}
+
+// processMessage: 수신한 메시지의 유형을 식별하고 해당 엔티티로 전송
+func processMessage(message *pb.GameMessage, conn net.Conn) {
+	switch msg := message.Message.(type) {
+	case *pb.GameMessage_PlayerPosition:
+		mg.GetPlayerManager().PlayerPosition(msg)
+	default:
+		log.Printf("Unexpected message type: %T", msg)
+	}
+}
